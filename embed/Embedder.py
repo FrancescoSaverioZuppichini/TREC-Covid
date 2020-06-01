4cb2322d61a376886fe3a7808bc736633d9d932b
Gabriele Sarti <gabriele.sarti996@gmail.com>
2020-06-01 12:57:29 +0200

    Added ELECTRA support in embedder, updated requirements

--- embed/Embedder.py
+++ embed/Embedder.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from Electra import ELECTRA
 from sentence_transformers import models, SentenceTransformer
 
 @dataclass
@@ -6,9 +7,10 @@ class Embedder:
     """
     This class uses Hugging's face transformers library to encode a list of strings into a 768 dim vetor.
     """
-    model_name: str = 'gsarti/scibert-nli'
+    model_name: str = 'gsarti/electra-msmarco-med-cord19-discriminator'
     max_seq_length: int  = 128
     do_lower_case: bool  = True
+    model_type: str = 'electra'
 
     def __post_init__(self):
         self.word_embedding_model = self.get_model()
@@ -18,11 +20,20 @@ class Embedder:
         self.model = SentenceTransformer(modules=[self.word_embedding_model, self.pooling_model])
     
     def get_model(self):
-        return models.BERT(
-            self.model_name,
-            max_seq_length=128,
-            do_lower_case=True
-        )
+        if self.model_type == 'electra':
+            return ELECTRA(
+                self.model_name,
+                max_seq_length=self.max_seq_length,
+                do_lower_case=self.do_lower_case
+            )
+        elif self.model_type == 'bert':
+            return models.BERT(
+                self.model_name,
+                max_seq_length=self.max_seq_length,
+                do_lower_case=self.do_lower_case
+            )
+        else:
+            raise AttributeError("Not supported")
 
     def get_pooling(self):
         return models.Pooling(self.word_embedding_model.get_word_embedding_dimension(),
