a8e294059460be76666c86977c9ef3731cc4d84a
Francesco <zuppif@usi.ch>
2020-06-01 09:58:55 +0200

    Embedder can be customized

--- embed/Embedder.py
+++ embed/Embedder.py
@@ -6,24 +6,30 @@ class Embedder:
     """
     This class uses Hugging's face transformers library to encode a list of strings into a 768 dim vetor.
     """
-    name: str = 'gsarti/scibert-nli'
+    model_name: str = 'gsarti/scibert-nli'
     max_seq_length: int  = 128
     do_lower_case: bool  = True
-    
+
     def __post_init__(self):
-        word_embedding_model = models.BERT(
-            'gsarti/biobert-nli',
+        self.word_embedding_model = self.get_model()
+        # apply pooling to get one fixed vector
+        self.pooling_model = self.get_pooling()
+
+        self.model = SentenceTransformer(modules=[self.word_embedding_model, self.pooling_model])
+    
+    def get_model(self):
+        return models.BERT(
+            self.model_name,
             max_seq_length=128,
             do_lower_case=True
         )
-        # apply pooling to get one fixed vector
-        pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
+
+    def get_pooling(self):
+        return models.Pooling(self.word_embedding_model.get_word_embedding_dimension(),
                 pooling_mode_mean_tokens=True,
                 pooling_mode_cls_token=False,
                 pooling_mode_max_tokens=False
             )
-    
-        self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-        
+
     def __call__(self, text: list):
         return self.model.encode(text) 
--- main.py
+++ main.py
@@ -3,12 +3,5 @@ from embed import Embedder
 from es import ElasticSearcher
 from elasticsearch import Elasticsearch
 
-print(text2art('COVID-19 Browser'))
-embedder = Embedder()
-es_searcher = ElasticSearcher()
-
-while True:
-    query = input('Type your question:')
-    query_emb = embedder([query])[0].tolist()
-    res = es_searcher(query_emb)
-    print(res)
+print(text2art('COVID-19 Trec'))
+print('TODO!')
